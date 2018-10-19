be4e540d0ee3bfec87bfd4321b40eae69b3df2b4
yachnytskyi <yachnytskyi1992@gmail.com>
2018-10-19 03:54:07 +0300

    Writed topic views and writed application_helper ( topic_author )

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def markdown_to_html(text)
+   Kramdown::Document.new(text, input: "GFM").to_html
+  end
+
+  def topic_author(topic)
+    user_signed_in? && current_user.id == topic.user_id
+  end
 end
